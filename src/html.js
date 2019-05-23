0a63aa129c923c29ca4bab391078fd230e912499
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-23 23:57:59 +0200

    feat: add agent support

--- src/html.js
+++ src/html.js
@@ -1,11 +1,13 @@
 'use strict'
 
+const debug = require('debug-logfmt')('html-microservice:html')
 const isUrl = require('is-url-http')
 const uaString = require('ua-string')
 const getHTML = require('html-get')
 
 const { HEADERS } = require('./constants')
 const cookies = require('./cookies')
+const { parseUri: parseProxyUri } = require('luminati-tunnel')
 
 module.exports = async (req, { prerender }) => {
   try {
@@ -14,11 +16,15 @@ module.exports = async (req, { prerender }) => {
     if (!targetUrl) return { showHelp: true }
     if (!isUrl(targetUrl)) return { url: targetUrl, invalidUrl: true }
 
+    const agent = req.query.proxy ? parseProxyUri(req.query.proxy) : undefined
+    debug('agent', agent)
+
     return getHTML(targetUrl, {
       prerender,
       gotOptions: {
         headers: {
           ...HEADERS,
+          agent,
           'user-agent': uaString,
           cookie: cookies.toString(targetUrl)
         }

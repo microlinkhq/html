25c98915cee232923a4cb40f647c36a0177786b8
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-08-01 23:52:24 +0200

    refactor: naming

--- src/index.js
+++ src/index.js
@@ -9,14 +9,13 @@ const { CACHE_TTL } = require('./constants')
 const html = require('./html')
 const send = require('./send')
 
-const applyMiddleware = (service, middlewares = []) => {
-  return middlewares
+const applyMiddleware = (service, middlewares = []) =>
+  middlewares
     .filter(Boolean)
     .reverse()
     .reduce((fn, nextMiddleware) => nextMiddleware(fn), service)
-}
 
-const decorate = fn => handler => (req, res, ...rest) => {
+const fromExpress = fn => handler => (req, res, ...rest) => {
   const next = () => handler(req, res, ...rest)
   return fn(req, res, next)
 }
@@ -29,6 +28,6 @@ const ssrCache = cacheableResponse({
 
 const fromCache = (req, res, opts) => ssrCache({ req, res, ...opts })
 
-const middlewares = [decorate(helmet)]
+const middlewares = [fromExpress(helmet)]
 
 module.exports = applyMiddleware(fromCache, middlewares)

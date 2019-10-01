080d0e42f551a361c50fefab5983ea21166dcebf
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-10-02 00:24:32 +0200

    build: require puppeteer explicity

--- src/html.js
+++ src/html.js
@@ -2,10 +2,23 @@
 
 const debug = require('debug-logfmt')('html-microservice:html')
 const { parseUri: parseProxyUri } = require('luminati-tunnel')
+const createBrowserless = require('browserless')
 const prependHttp = require('prepend-http')
 const uaString = require('ua-string')
 const isUrl = require('is-url-http')
 const getHTML = require('html-get')
+
+const { NODE_ENV = 'development' } = process.env
+
+const isProduction = NODE_ENV === 'production'
+
+const puppeteer = isProduction ? require('puppeteer-core') : require('puppeteer')
+
+const puppeteerDevices = isProduction
+  ? require('puppeteer-core/DeviceDescriptors')
+  : require('puppeteer/DeviceDescriptors')
+
+const getBrowserless = createBrowserless({ puppeteer, puppeteerDevices })
 
 const { HEADERS } = require('./constants')
 const cookies = require('./cookies')
@@ -21,6 +34,7 @@ module.exports = async req => {
     debug('agent', agent)
 
     return getHTML(targetUrl, {
+      getBrowserless,
       prerender,
       gotOptions: {
         headers: {

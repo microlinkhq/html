dbb38b67992ccb2e84e77834d06cbfe0eb2d8113
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-11-12 08:55:37 +0000

    build: update mem cacheKey

--- src/html/get-headers.js
+++ src/html/get-headers.js
@@ -10,19 +10,24 @@ const parseUrl = require('./parse-url')
 
 const normalizeHeaders = headers => mapKeys(headers, (value, key) => toLower(headerCase(key)))
 
-module.exports = mem((url, headers) => {
-  return pickBy({
-    'user-agent': uaString,
-    'accept-language': 'en-us',
-    connection: 'keep-alive',
-    // TODO: Brotli (br) support requires Node.js 11.7.0 or later.
-    'accept-encoding': 'gzip, deflate',
-    'cache-control': 'no-cache',
-    'upgrade-insecure-requests': '1',
-    accept: '*/*',
-    pragma: 'no-cache',
-    origin: parseUrl(url).origin,
-    cookie: getCookies(url),
-    ...normalizeHeaders(headers)
-  })
-})
+module.exports = mem(
+  (url, headers) => {
+    return pickBy({
+      'user-agent': uaString,
+      'accept-language': 'en-us',
+      connection: 'keep-alive',
+      // TODO: Brotli (br) support requires Node.js 11.7.0 or later.
+      'accept-encoding': 'gzip, deflate',
+      'cache-control': 'no-cache',
+      'upgrade-insecure-requests': '1',
+      accept: '*/*',
+      pragma: 'no-cache',
+      origin: parseUrl(url).origin,
+      cookie: getCookies(url),
+      ...normalizeHeaders(headers)
+    })
+  },
+  {
+    cacheKey: JSON.sttringify
+  }
+)
